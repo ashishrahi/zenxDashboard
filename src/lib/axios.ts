d84008d08030887ff1a9da5f64877730633787f3
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-09-26 00:03:31 +0530

    updated code

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -38,7 +38,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "multipart/form-data",
   },
 });
 
